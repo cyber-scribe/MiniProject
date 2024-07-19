dcb3822dd77219df9bb6dc032756032e18976c5a
cyber-scribe <vd297520@gmail.com>
2024-07-19 21:20:15 +0530

    faq section

---
+++ main.js
@@ -0,0 +1,33 @@
+const faqHeaders = document.querySelectorAll(".faqs-container .faq-header");
+
+faqHeaders.forEach((header, i) => {
+  header.addEventListener("click", () => {
+    header.nextElementSibling.classList.toggle("active");
+
+    const open = header.querySelector(".open");
+    const close = header.querySelector(".close");
+
+    if (header.nextElementSibling.classList.contains("active")) {
+      open.classList.remove("active");
+      close.classList.add("active");
+    } else {
+      open.classList.add("active");
+      close.classList.remove("active");
+    }
+  });
+});
+
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= itemWidth + padding
+})
+
+next.addEventListener('click',()=>{
+  list.scrollLeft += itemWidth + padding
+})
